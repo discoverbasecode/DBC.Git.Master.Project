9727e5346dc3cafa5adc297d7e5178526ffdaf31
Discoverbasecode <Discoverbasecode@gmail.com>
2025-07-23 19:58:53 +0330

    init console app dbc Git tools

---
+++ DBC.Git.Master.App/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using TGui = Terminal.Gui;
+using Octokit;
+
+namespace DBC.Git.Master.App
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+                Console.WriteLine("Hiiiii !");
+        }
+    }
+}
